4a9f041061122d9a6f827aa08d5061228042b6c3
ProsperOjonimi <jaypee5699@yahoo.com>
2025-10-02 01:01:02 +0100

    Added welcome screen feature and search bar effect

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 "use strict";
+const initialBackground = document.querySelector(".whatsapp-open");
+const inputBar = document.querySelector(".search-space");
+const searchBar = document.querySelector(".chat-list_search-bar");
+
+class App {
+  constructor() {
+    inputBar.addEventListener("focus", this.focusSearchBar.bind(this));
+    inputBar.addEventListener("blur", this.blurSearchBar.bind(this));
+  }
+  focusSearchBar() {
+    console.log("focus");
+    inputBar.style.backgroundColor = "#FFFFFF";
+    searchBar.style.backgroundColor = "#FFFFFF";
+    searchBar.style.border = "2px solid #1DAA61";
+  }
+  blurSearchBar() {
+    console.log("blur");
+    inputBar.style.backgroundColor = "#f6f5f3";
+    searchBar.style.backgroundColor = "#f6f5f3";
+    searchBar.style.border = "none";
+  }
+  init() {
+    setTimeout(() => {
+      initialBackground.classList.add("hidden");
+    }, 1500);
+  }
+}
+
+const app = new App();
+app.init();
