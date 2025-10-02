f8c4bc838db6f6d9e015478cfe96b8ab5319b930
ProsperOjonimi <jaypee5699@yahoo.com>
2025-10-02 14:23:03 +0100

    update on project architecture

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,10 @@
 "use strict";
 import { app } from "./view.js";
+import { chatList } from "./view.js";
+import { chats } from "./model.js";
 
 const initializeApp = function () {
   app.init();
+  chatList.renderMarkup(chats);
 };
 initializeApp();
--- js/view.js
+++ js/view.js
@@ -1,7 +1,61 @@
 const initialBackground = document.querySelector(".whatsapp-open");
 const inputBar = document.querySelector(".search-space");
 const searchBar = document.querySelector(".chat-list_search-bar");
+const chatListContainer = document.querySelector(".chat-list_chats");
 
+class ChatList {
+  constructor() {
+    chatListContainer.innerHTML = "";
+  }
+  renderMarkup(chats) {
+    chats.forEach((obj) => {
+      const renderedMessageArr =
+        obj.msgRecieved.length === 0 ? obj.msgSent : obj.msgRecieved;
+      const renderCheckmark = function () {
+        if (renderedMessageArr === obj.msgSent && obj.seen === false) {
+          return '<ion-icon name="checkmark-done-outline" class="checkmark-icon"></ion-icon>';
+        } else if (renderedMessageArr === obj.msgSent && obj.seen === true) {
+          return '<ion-icon name="checkmark-done-outline" class="checkmark-icon checkmark-icon_blue"></ion-icon>';
+        } else {
+          return;
+        }
+      };
+      const html = `
+    <a href="" class="chat-list-link">
+          <div class="chat-list_proper">
+            <div class="message-profile-img">
+              <ion-icon name="person" class="person-icon"></ion-icon>
+            </div>
+          <div class="message-description">
+              <p class="person-name">${obj.chatName}</p>
+              <p class="person-text">${renderCheckmark()}${
+        renderedMessageArr[0]
+      }</p>
+
+          </div>
+          <div class="message-information">
+            <p class="message-time ${
+              (renderedMessageArr === obj.msgRecieved && obj.seen === true) ||
+              renderedMessageArr === obj.msgSent
+                ? "greyColor"
+                : ""
+            }">${obj.timeOfLastChat}</p>
+          <div class="message-no  ${
+            (renderedMessageArr === obj.msgRecieved && obj.seen === true) ||
+            renderedMessageArr === obj.msgSent
+              ? "hidden"
+              : ""
+          }" ">${renderedMessageArr.length}</div>
+          </div>
+
+          </div>
+          </a>
+    
+    `;
+      chatListContainer.insertAdjacentHTML("beforeend", html);
+    });
+  }
+}
 class App {
   constructor() {
     inputBar.addEventListener("focus", this.focusSearchBar.bind(this));
@@ -27,3 +81,4 @@ class App {
 }
 
 export const app = new App();
+export const chatList = new ChatList();
